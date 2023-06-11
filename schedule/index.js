92ceedd9c4fb1eae678d8e554e54f71beed8a06c
monosniper <vanteateam@gmail.com>
2023-06-12 02:28:41 +0500

    das

--- schedule/index.js
+++ schedule/index.js
@@ -10,9 +10,9 @@ exports.initScheduledJobs = async () => {
         const targets = await TargetModel.find({isActive: true})
 
         targets.forEach(async target => {
-            target.clicks = target.clicks + (getRandomInt(0, 10) * (target.amount / 100))
-            target.visits = target.visits + (getRandomInt(0, 3) * (target.amount / 100))
-            target.online = (getRandomInt(0, 25) * (target.amount / 100))
+            target.clicks = target.clicks + (getRandomInt(0, 10) * (target.amount / 100)).toFixed()
+            target.visits = target.visits + (getRandomInt(0, 3) * (target.amount / 100)).toFixed()
+            target.online = (getRandomInt(0, 25) * (target.amount / 100)).toFixed()
             target.middleTime = getRandomInt(1, 10)
 
             await target.save()
